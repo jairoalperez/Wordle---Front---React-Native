9234903867edbf0eb0eb2d019e6e506e49e8d514
jairoalperez <jairoalberto98@hotmail.com>
2022-07-27 17:38:00 -0400

    lo lograremos

--- App.js
+++ App.js
@@ -1,25 +1,3 @@
-<<<<<<< Updated upstream
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-=======
 import React from "react";
 import {NavigationContainer} from '@react-navigation/native'
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -48,4 +26,3 @@ export default function App () {
       </NavigationContainer>
   )
 }
->>>>>>> Stashed changes
