1070ef9de798dedab927129290d8e9bfc2328fe3
jairoalperez <jairoalberto98@hotmail.com>
2022-07-27 17:29:58 -0400

    lo lograremos

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -18,3 +19,33 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+=======
+import React from "react";
+import {NavigationContainer} from '@react-navigation/native'
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Home from './screens/Home'
+import Login from './screens/Login'
+import Register from "./screens/Register";
+import Verificacion from "./screens/Verificacion";
+import Hub from "./screens/Hub"
+import Juego from "./screens/Juego"
+
+const Stack = createNativeStackNavigator()
+
+export default function App () {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{headerShown: false}}>
+          <Stack.Screen name='Home' component={Home}/>
+          <Stack.Screen name='Login' component={Login}/>
+          <Stack.Screen name='Register' component={Register}/>
+          <Stack.Screen name='Verificacion' component={Verificacion}/>
+          <Stack.Screen name='Hub' component={Hub}/>
+          <Stack.Screen name='Juego' component={Juego}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+  )
+}
+>>>>>>> Stashed changes
